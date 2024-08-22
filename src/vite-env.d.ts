6cc05554c07a4340650f1eabe179ffdebf2cec11
jiwnchoi <jiwnchoi@skku.edu>
2024-08-22 15:55:06 +0900

    fix pwa

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,6 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-pwa/client" />
+/// <reference types="vite-plugin-pwa/react" />
+/// <reference types="vite-plugin-pwa/info" />
+
 type ObjectWithKey<T> = T & { key: string };
