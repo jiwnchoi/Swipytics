c5865c3eb50289e1641dca63d4c1b56179994f8d
jiwnchoi <jiwnchoi@skku.edu>
2024-08-23 13:26:01 +0900

    test

--- api/app.py
+++ api/app.py
@@ -3,24 +3,27 @@ import numpy as np
 import pandas as pd
 import pydantic
 import sklearn
-from api.functions import load_data
-from api.models import Chart, Session, State
-from draco import Draco
-from draco.renderer import AltairRenderer
 
-drc = Draco()
+try:
+  from api.functions import load_data
+  from api.models import Chart, Session, State
+  from draco import Draco
+  from draco.renderer import AltairRenderer
 
-state = State()
+  drc = Draco()
 
+  state = State()
 
-# JavaScript에서 호출하는 함수를 CamelCase로 정의
-def loadData(filename: str):
-  load_data(state, filename)
-  print(f"Data Loaded: {state.filename}")
-  print(f"Data Shape: {state.df.shape}")
-  print(state.df.head())
+  # JavaScript에서 호출하는 함수를 CamelCase로 정의
+  def loadData(filename: str):
+    load_data(state, filename)
+    print(f"Data Loaded: {state.filename}")
+    print(f"Data Shape: {state.df.shape}")
+    print(state.df.head())
 
-
-print(
-  f"Package Loaded. draco: {draco.__version__}, numpy: {np.__version__}, pandas: {pd.__version__}, sklearn: {sklearn.__version__},  pydantic: {pydantic.__version__}"
-)
+  print(
+    f"Package Loaded. draco: {draco.__version__}, numpy: {np.__version__}, pandas: {pd.__version__}, sklearn: {sklearn.__version__},  pydantic: {pydantic.__version__}"
+  )
+except Exception as e:
+  print(f"Error: {e}")
+  raise e
