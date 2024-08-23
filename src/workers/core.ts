1d8feb7e23850f4a64f559c02132ff572a2b78a6
jiwnchoi <jiwnchoi@skku.edu>
2024-08-23 13:22:34 +0900

    fix vite error

--- src/workers/core.ts
+++ src/workers/core.ts
@@ -1,4 +1,5 @@
 import { runEntryPointAsync, setupPyodideFiles } from "virtual:pyodide-files";
+
 import type { PyodideInterface } from "pyodide";
 
 let pyodide: PyodideInterface | null = null;
@@ -8,14 +9,20 @@ async function loadEnvs(): Promise<PyodideInterface> {
     return pyodide;
   }
   const { loadPyodide } = await import("pyodide");
-  console.log(`Loading Pyodide from ${import.meta.env.BASE_URL}/artifacts`);
-  pyodide = await loadPyodide({
-    indexURL: `${import.meta.env.BASE_URL}/artifacts`,
-  });
-  await pyodide.loadPackage(["draco"]);
-  await setupPyodideFiles(pyodide);
-  await runEntryPointAsync(pyodide);
-
-  return pyodide;
+  try {
+    // biome-ignore lint/nursery/noConsole: <explanation>
+    console.log(`Loading Pyodide from ${import.meta.env.BASE_URL}/artifacts`);
+    pyodide = await loadPyodide({
+      indexURL: `${import.meta.env.BASE_URL}/artifacts`,
+    });
+    await pyodide.loadPackage(["draco"]);
+    await setupPyodideFiles(pyodide);
+    await runEntryPointAsync(pyodide);
+    return pyodide;
+  } catch (e) {
+    // biome-ignore lint/nursery/noConsole: <explanation>
+    console.error(e);
+    throw e;
+  }
 }
 export { loadEnvs };
