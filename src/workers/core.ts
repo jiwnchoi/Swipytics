8bdb6d5873c52409bb7d86b61d0d2eacb3632c58
jiwnchoi <jiwnchoi@skku.edu>
2024-08-23 13:47:22 +0900

    remove try catch

--- src/workers/core.ts
+++ src/workers/core.ts
@@ -9,22 +9,16 @@ async function loadEnvs(): Promise<PyodideInterface> {
     return pyodide;
   }
   const { loadPyodide } = await import("pyodide");
-  try {
-    // biome-ignore lint/nursery/noConsole: <explanation>
-    console.log("Loading Envs");
-    // biome-ignore lint/nursery/noConsole: <explanation>
-    console.log("Loading Pyodide from https://jiwnchoi.github.io/Swipytics/artifacts");
-    pyodide = await loadPyodide({
-      indexURL: "https://jiwnchoi.github.io/Swipytics/artifacts",
-    });
-    await pyodide.loadPackage(["draco"]);
-    await setupPyodideFiles(pyodide);
-    await runEntryPointAsync(pyodide);
-    return pyodide;
-  } catch (e) {
-    // biome-ignore lint/nursery/noConsole: <explanation>
-    console.error(e);
-    throw e;
-  }
+  // biome-ignore lint/nursery/noConsole: <explanation>
+  console.log("Loading Envs");
+  // biome-ignore lint/nursery/noConsole: <explanation>
+  console.log("Loading Pyodide from https://jiwnchoi.github.io/Swipytics/artifacts");
+  pyodide = await loadPyodide({
+    indexURL: "https://jiwnchoi.github.io/Swipytics/artifacts",
+  });
+  await pyodide.loadPackage(["draco"]);
+  await setupPyodideFiles(pyodide);
+  await runEntryPointAsync(pyodide);
+  return pyodide;
 }
 export { loadEnvs };
