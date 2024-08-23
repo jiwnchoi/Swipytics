58ed3048087b41dcc2e8ace7b3dddec5b9345f92
jiwnchoi <jiwnchoi@skku.edu>
2024-08-23 13:50:48 +0900

    temporily remove pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 import pyodidePlugin from "vite-plugin-pyodide";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -16,15 +15,15 @@ export default defineConfig({
     }),
     react(),
     tsconfigPaths(),
-    VitePWA({
-      workbox: {
-        maximumFileSizeToCacheInBytes: 1024 * 1024 * 1024,
-      },
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
-      },
-      manifest: false,
-    }),
+    // VitePWA({
+    //   workbox: {
+    //     maximumFileSizeToCacheInBytes: 1024 * 1024 * 1024,
+    //   },
+    //   registerType: "autoUpdate",
+    //   devOptions: {
+    //     enabled: true,
+    //   },
+    //   manifest: false,
+    // }),
   ],
 });
