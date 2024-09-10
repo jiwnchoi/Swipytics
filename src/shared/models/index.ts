5ea192a8a02a8fd5b948a49c10a0b22406a517b1
jiwnchoi <jiwnchoi@skku.edu>
2024-09-11 00:22:41 +0900

    feat(src): register types in index

--- src/shared/models/index.ts
+++ src/shared/models/index.ts
@@ -3,3 +3,6 @@ export type { default as TDemo } from "./TDemo";
 export type { default as TSession } from "./TSession";
 export type { default as TState } from "./TState";
 export type { default as TSupportedDataType } from "./TSupportedDataType";
+export type { default as TDataField } from "./TDataField";
+export type { default as TFieldType } from "./TFieldType";
+export type { default as TMetadata } from "./TMetadata";
