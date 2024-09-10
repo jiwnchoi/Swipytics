58f0682e5732d9bdd6ccdcb2cff6e195b0ebcb3b
jiwnchoi <jiwnchoi@skku.edu>
2024-09-11 01:18:12 +0900

    feat(src): refactor to accordian

--- src/components/MetadataFields.tsx
+++ src/components/MetadataFields.tsx
@@ -1,58 +1,86 @@
-import { Box, Flex, Grid, type StackProps, TabPanel, Text, VStack } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Badge,
+  Flex,
+  Grid,
+  Heading,
+  Icon,
+  type IconProps,
+  Spacer,
+  type StackProps,
+  TabPanel,
+  type TabPanelProps,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import type { TDataField, TFieldType, TMetadata } from "@shared/models";
+import { scrollbarStyle } from "@shared/theme";
+import { useSessionsStore } from "@stores";
 import { format } from "d3-format";
-import { Calendar03Icon, GridIcon, StarIcon, Tag01Icon } from "hugeicons-react";
-import type TDataField from "../shared/models/TDataField";
-import type TFieldType from "../shared/models/TFieldType";
-import type TMetadata from "../shared/models/TMetadata";
-import { useSessionsStore } from "../stores";
+import { Calendar03Icon, GridIcon, Tag01Icon, TextFontIcon } from "hugeicons-react";
 
-const FieldIcon = ({ metadataType }: { metadataType: TFieldType }) => {
+const FieldIcon = ({ metadataType, ...props }: IconProps & { metadataType: TFieldType }) => {
   switch (metadataType) {
     case "numeric":
-      return <GridIcon size="16px" />;
+      return <Icon as={GridIcon} {...props} />;
     case "categorical":
-      return <StarIcon size="16px" />;
+      return <Icon as={TextFontIcon} {...props} />;
     case "datetime":
-      return <Calendar03Icon size="16px" />;
+      return <Icon as={Calendar03Icon} {...props} />;
     case "name":
-      return <Tag01Icon size="16px" />;
+      return <Icon as={Tag01Icon} {...props} />;
     default:
       return null;
   }
 };
+
+const formatter = format(".3~s");
+
+// Badge 너무 구리다 디자인 수정하기
+function MetadataKeyBadge({ children }: { children: string }) {
+  return (
+    <Flex flexDir={"column"} justify={"center"} alignItems={"start"} w="full" h="full">
+      <Badge>{children}</Badge>
+    </Flex>
+  );
+}
 
 function EachMetaData({ metadata }: { metadata: TMetadata }) {
   switch (metadata.type) {
     case "datetime":
       return (
         <Grid gridTemplateColumns="1fr 4fr">
-          <Text>min</Text>
+          <MetadataKeyBadge>min</MetadataKeyBadge>
           <Text>{metadata.min}</Text>
-          <Text>max</Text>
+          <MetadataKeyBadge>max</MetadataKeyBadge>
           <Text>{metadata.max}</Text>
         </Grid>
       );
     case "numeric":
       return (
         <Grid gridTemplateColumns="2fr 3fr 2fr 3fr">
-          <Text>min</Text>
-          <Text>{format("~s")(metadata.min)}</Text>
-          <Text>max</Text>
-          <Text>{format("~s")(metadata.max)}</Text>
-          <Text>mean</Text>
-          <Text>{format("~s")(metadata.mean)}</Text>
-          <Text>median</Text>
-          <Text>{format("~s")(metadata.median)}</Text>
-          <Text>std</Text>
-          <Text>{format("~s")(metadata.std)}</Text>
+          <MetadataKeyBadge>min</MetadataKeyBadge>
+          <Text>{formatter(metadata.min)}</Text>
+          <MetadataKeyBadge>max</MetadataKeyBadge>
+          <Text>{formatter(metadata.max)}</Text>
+          <MetadataKeyBadge>mean</MetadataKeyBadge>
+          <Text>{formatter(metadata.mean)}</Text>
+          <MetadataKeyBadge>median</MetadataKeyBadge>
+          <Text>{formatter(metadata.median)}</Text>
+          <MetadataKeyBadge>std</MetadataKeyBadge>
+          <Text>{formatter(metadata.std)}</Text>
         </Grid>
       );
     case "categorical":
       return (
         <Grid gridTemplateColumns="1fr 4fr">
-          <Text>top</Text>
+          <MetadataKeyBadge>top</MetadataKeyBadge>
           <Text>{metadata.top}</Text>
-          <Text>freq</Text>
+          <MetadataKeyBadge>freq</MetadataKeyBadge>
           <Text>{metadata.freq}</Text>
         </Grid>
       );
@@ -64,31 +92,40 @@ function EachMetaData({ metadata }: { metadata: TMetadata }) {
 
 function EachField({ field }: { field: TDataField }) {
   return (
-    <Box w="100%">
-      <Flex alignItems="center" gap="8px" bgColor="gray.100" p="2px" borderRadius="2px">
-        <FieldIcon metadataType={field.type} /> <Text>{field.name}</Text>
-        <Text> (total: {field.metadata.count})</Text>
-      </Flex>
-      <Grid gridTemplateColumns="2fr 3fr 2fr 3fr">
-        <Text>unique</Text>
-        <Text>{field.metadata.unique}</Text>
-        <Text>missing</Text>
-        <Text>{field.metadata.missing}</Text>
-      </Grid>
-      <EachMetaData metadata={field.metadata} />
-    </Box>
+    <AccordionItem>
+      <AccordionButton borderRadius={"md"}>
+        <Flex w="full" gap={2} align={"center"}>
+          <FieldIcon metadataType={field.type} boxSize={4} />
+          <Heading fontSize="md">{field.name}</Heading>
+          <Spacer />
+          <AccordionIcon />
+        </Flex>
+      </AccordionButton>
+      <AccordionPanel px={2} pb={2}>
+        <Grid gridTemplateColumns="2fr 3fr 2fr 3fr">
+          <MetadataKeyBadge>unique</MetadataKeyBadge>
+          <Text>{field.metadata.unique}</Text>
+          <MetadataKeyBadge>missing</MetadataKeyBadge>
+          <Text>{field.metadata.missing}</Text>
+        </Grid>
+        <EachMetaData metadata={field.metadata} />
+      </AccordionPanel>
+    </AccordionItem>
   );
 }
 
-function MetadataFields(props: StackProps) {
+function MetadataFields(props: TabPanelProps & StackProps) {
   const fields = useSessionsStore((state) => state.fields);
 
   return (
-    <TabPanel as={VStack} {...props}>
-      {fields.length === 0 && <Text>Load data</Text>}
-      {fields.map((field) => (
-        <EachField key={field.name} field={field} />
-      ))}
+    <TabPanel as={VStack} h="full" {...props}>
+      <Flex h="full" w="full" sx={scrollbarStyle} overflowY="auto" flexDir="column">
+        <Accordion allowMultiple>
+          {fields.map((field) => (
+            <EachField key={field.name} field={field} />
+          ))}
+        </Accordion>
+      </Flex>
     </TabPanel>
   );
 }
